9cfc0b6101753ae4e8d3c697fb4b9ba50d7cf85b
Muhammad Umair <mumair5393@gmail.com>
2025-02-15 13:32:24 +0100

    Functional test now checks we can input a to-do item

--- superlist/functional-tests.py
+++ superlist/functional-tests.py
@@ -1,5 +1,9 @@
+from django.template.defaultfilters import time
 from selenium import webdriver
 import unittest
+
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
 
 class NewUserTest(unittest.TestCase):
@@ -12,18 +16,32 @@ class NewUserTest(unittest.TestCase):
     def test_user_flow(self):
         # User visits the website and notices "To-Do" in the title
         self.browser.get("http://localhost:8000")
+
+        # She notices the page title and header mention to-do lists
         self.assertIn("To-Do", self.browser.title)
+        header_text = self.browser.find_element(By.TAG_NAME, "h1").text
+        self.assertIn("To-Do", header_text)
 
-        # Currently there is only a text box with an add button with it
-        # User adds Task 1 and click add button.
+        # She is invited to enter a to-do item straight away
+        input_box = self.browser.find_element(By.ID, "id_new_item")
+        self.assertEqual(input_box.get_attribute("placeholder"), "Enter a to-do item")
+        
+        # She enters the first to-do item
+        input_box.send_keys("Buy peacock feathers.")
+        
+        # When she hits enter the page updates and now the page has a to-do item on the page.
+        input_box.send_keys(Keys.ENTER)
+        time.sleep(1)
+        table = self.browser.find_element(By.ID, "id_list_table")
+        rows = table.find_elements(By.TAG_NAME, "tr")
+        self.assertTrue(any(row.text == '1: Buy peacock feathers' for row in rows) )
+        
 
+        # There is still a text box inviting her to add another item. She 
+        # enters "Use peacock feathers to make a fly" (Edith is very methodical)
+        self.fail('Finish the test!')
+        # The page updates again, and now shows both items on her list
 
-        # User sees one Task 1 in the to-do list and also the text box
-        # with an add button
-
-        # Now there should be single to-do item
-        # User adds Task 2 and click add button.
-        self.fail("Finish the test!")
 
 if __name__ == '__main__':
     unittest.main()
