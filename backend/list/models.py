3254fe88f88607c626f4e51c36c75a70f313f425
Muhammad Umair <mumair5393@gmail.com>
2025-02-17 22:21:45 +0100

    adding Item model in the app

--- backend/list/models.py
+++ backend/list/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+
+class Item(models.Model):
+    id = models.AutoField(primary_key=True)
+    text = models.TextField(blank=True, null=True)
--- backend/list/tests.py
+++ backend/list/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.urls import resolve
 from list.views import home_page
+from list.models import Item
 
 # Create your tests here.
 class HomePageTest(TestCase):
@@ -18,4 +19,21 @@ class HomePageTest(TestCase):
         self.assertTemplateUsed(response, 'home.html')
 
 
+class ItemModelTest(TestCase):
+    def test_saving_and_retrieving_items(self):
+        first_item = Item()
+        first_item.text = 'List item one'
+        first_item.save()
+        
+        second_item = Item()
+        second_item.text = 'List item two'
+        second_item.save()
+        
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+        self.assertEqual(saved_items[0].text, 'List item one')
+        self.assertEqual(saved_items[1].text, 'List item two')
+        
+        
+        
         
